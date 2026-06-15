a36119df7796bf804834eeaf9dbfcff3aef87497
mitmac29 <miteshjandj@gmail.com>
2026-06-15 18:25:46 +0000

    fixxup vssh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,13 +4,13 @@
 # set -e
 
 echo "Cloning device trees..."
-git clone https://github.com/mitmac29/device_xiaomi_sdm660-common.git -b vos device/xiaomi/sdm660-common
+git clone https://github.com/mitmac29/android_device_xiaomi_sdm660-common.git -b vos device/xiaomi/sdm660-common
 
 echo "Cloning vendor trees..."
 git clone https://github.com/pix106/android_vendor_xiaomi_sdm660-common.git -b 16.1 vendor/xiaomi/sdm660-common
 git clone https://github.com/pix106/android_vendor_xiaomi_lavender.git -b 16.1 vendor/xiaomi/lavender
 
 echo "Cloning kernel..."
-git clone https://github.com/mitmac29/kernel_xiaomi_sdm660.git -b vos kernel/xiaomi/sdm660
-
+git clone https://github.com/mitmac29/android_kernel_xiaomi_sdm660_southwest-ng.git -b vos kernel/xiaomi/sdm660
+git clone https://github.com/mitmac29/V4.git -b bq1 packages/apps/ViPER4AndroidFX
 echo "✅ All repos cloned successfully!"
