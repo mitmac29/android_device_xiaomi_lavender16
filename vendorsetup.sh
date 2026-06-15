ecad512568805950ee7fbbada86cb95e66db82f4
Crave <aosp@crave.io>
2026-06-15 19:10:35 +0000

    fix vssh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ echo "Cloning device trees..."
 git clone https://github.com/mitmac29/android_device_xiaomi_sdm660-common.git -b vos device/xiaomi/sdm660-common
 
 echo "Cloning vendor trees..."
-git clone https://github.com/pix106/android_vendor_xiaomi_sdm660-common.git -b 16.1 vendor/xiaomi/sdm660-common
-git clone https://github.com/pix106/android_vendor_xiaomi_lavender.git -b 16.1 vendor/xiaomi/lavender
+git clone https://github.com/pix106/android_vendor_xiaomi_sdm660-common.git -b 16.2 vendor/xiaomi/sdm660-common
+git clone https://github.com/pix106/android_vendor_xiaomi_lavender.git -b 16.2 vendor/xiaomi/lavender
 
 echo "Cloning kernel..."
 git clone https://github.com/mitmac29/android_kernel_xiaomi_sdm660_southwest-ng.git -b vos kernel/xiaomi/sdm660
